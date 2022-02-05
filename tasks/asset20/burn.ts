7441b59a37aa67685eaefa11053ed64717c8df0b
Alexander <v0xt.alex@gmail.com>
2022-02-05 14:44:04 +0300

    add erc20 tasks

---
+++ tasks/asset20/burn.ts
@@ -0,0 +1,29 @@
+import fs from "fs";
+import dotenv from "dotenv";
+import { task, types } from "hardhat/config";
+
+task("burn", "Burn tokens on provided account")
+  .addParam("amount", "The amount of tokens to burn", 0, types.int)
+  .addParam("from", "The address to burn from")
+  .addOptionalParam("token", "Token contract address. By default grab it from .env")
+  .setAction(async (taskArgs, hre) => {
+    const network = hre.network.name;
+    const envConfig = dotenv.parse(fs.readFileSync(`.env-${network}`));
+    for (const parameter in envConfig) {
+      process.env[parameter] = envConfig[parameter];
+    }
+
+    const token = await hre.ethers.getContractAt(
+      process.env.TOKEN_NAME as string,
+      taskArgs.token || (process.env.ASSET20_TOKEN_ADDRESS as string)
+    );
+
+    const amount = hre.ethers.utils.parseUnits(
+      taskArgs.amount,
+      process.env.TOKEN_DECIMALS
+    );
+
+    console.log(`\nBurning ${taskArgs.amount} tokens from ${taskArgs.from}...\n`);
+    await token.burn(taskArgs.from, amount);
+    console.log(`Tx sent!`);
+  });
---
+++ tasks/asset20/grantRole.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+import dotenv from "dotenv";
+import { task } from "hardhat/config";
+
+import { roles } from "../../test/utils";
+
+task("grantRole", "Grants role to account")
+  .addParam(
+    "role",
+    "Available roles: 'minter', 'burner'. Minter by default",
+    roles.minter
+  )
+  .addParam("to", "Address to grant role to")
+  .addOptionalParam("token", "The address of the Token. By default grab it from .env")
+  .setAction(async (taskArgs, hre) => {
+    const network = hre.network.name;
+    const envConfig = dotenv.parse(fs.readFileSync(`.env-${network}`));
+    for (const parameter in envConfig) {
+      process.env[parameter] = envConfig[parameter];
+    }
+
+    const token = await hre.ethers.getContractAt(
+      process.env.TOKEN_NAME as string,
+      taskArgs.token || (process.env.ASSET20_TOKEN_ADDRESS as string)
+    );
+
+    const role = taskArgs.role === "burner" ? roles.burner : roles.minter;
+
+    console.log(`\nGranting role ${role} to ${taskArgs.to}...\n`);
+    await token.grantRole(role, taskArgs.to);
+    console.log(`Tx sent!`);
+  });
